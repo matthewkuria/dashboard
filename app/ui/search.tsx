a2f04d842040648e69fa8ba697ef1210692e321b
matthewkuria <mathewkwachira@gmail.com>
2024-05-07 21:03:35 +0300

    add more documentation to the search component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,6 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
+    // Create a new Instance of URl params
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
