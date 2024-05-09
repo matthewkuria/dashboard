85ab0b77a6a4253065c18f1cad697ccf7a971782
matthewkuria <mathewkwachira@gmail.com>
2024-05-09 22:44:06 +0300

    add promise.all to fetch both invoices nd customers in parallel

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -5,6 +5,11 @@ import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
  
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
+    // Use promise.all to fetch the invoice and customers in parallel
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers(),
+      ]);
   return (
     <main>
       <Breadcrumbs
