0cb4d6df2b31c9361e7b5eacee9a83fe7bcc6b19
matthewkuria <mathewkwachira@gmail.com>
2024-05-15 21:58:12 +0300

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 export default function RootLayout({
