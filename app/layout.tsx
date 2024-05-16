01ada5315f05f3419f237a660ff9e226e314d115
matthewkuria <mathewkwachira@gmail.com>
2024-05-16 10:20:19 +0300

    add title template on app/layout to be used on every page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
-  description: 'The official Next.js Course Dashboard, built with App Router.',
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 import '@/app/ui/global.css';
