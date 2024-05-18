28af57a6650810492a08e6cca8aff956ebee2163
matthewkuria <mathewkwachira@gmail.com>
2024-05-18 12:16:57 +0300

    chnage title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: {
     template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    default: 'Acme Dashboard done by matt',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
